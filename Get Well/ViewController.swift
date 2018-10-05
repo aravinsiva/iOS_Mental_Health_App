f335c8b0af947a0d08f2b0fbeb6160a7b646f3b5
aravin.siva <aravinarjunn@gmail.com>
2018-10-05 11:54:33 -0400

    UI_Created

---
+++ Get Well/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get Well
+//
+//  Created by Aravin Sivakumar on 2018-10-05.
+//  Copyright © 2018 Aravin Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
